fbf8fa6195358a7335cc748df85b897467568c1f
Sachidananda Urs <sacchi@gmail.com>
2022-01-20 07:00:47 +0530

    s/syscall/call/

--- unix/C/cheap_daemon.c
+++ unix/C/cheap_daemon.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /* Cheap daemon: This program illustrates daemonizing without using the
- * daemon(3) syscall. This is a very lazy one, doesn't change the working dir
+ * daemon(3) call. This is a very lazy one, doesn't change the working dir
  * or redirect the stdout, stderr, or stdin. It just returns from the process
- * which is holding the terminal (i.e parent), the child carries on. */
+ * which is holding the terminal (i.e parent), and the child carries on. */
 
 void infinite_loop(void);
 
